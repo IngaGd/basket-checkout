4691a1749427602489147ad502ed28c43a484795
IngaGd <inga.gudaite@gmail.com>
2025-02-08 17:22:59 +0700

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Basket from './components/Basket';
-import Checkout from './components/Checkout';
-import ProductList from './components/ProductList';
-import Header from './components/Header';
+import "./styles/main.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Basket from "./components/Basket";
+import Checkout from "./components/Checkout";
+import ProductList from "./components/ProductList";
+import Header from "./components/Header";
 
 function App() {
-    return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<ProductList />} />
-                <Route path="/basket" element={<Basket />} />
-                <Route path="/checkout" element={<Checkout />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/basket" element={<Basket />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
