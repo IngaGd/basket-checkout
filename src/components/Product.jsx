bde7fc52a744ec9da7529949cc322efe9e393420
IngaGd <inga.gudaite@gmail.com>
2025-02-09 15:13:46 +0700

    Refactor

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -10,7 +10,11 @@ export default function Product({
       <div>Product: {title}</div>
       <div>Price: {price} $</div>
       <div className="image-container">
-        <img src={img} alt={title} className="image" />
+        <img
+          src={`${process.env.PUBLIC_URL}${img}`}
+          alt={title}
+          className="image"
+        />
       </div>
       {clickHandle && (
         <button className="btn" onClick={clickHandle}>
